69a9ae494203bfdc83b6652e8469aeb35de630d7
OMFrydenlund <migfrydenlund@gmail.com>
2024-02-19 11:33:41 +0100

    Added conversion logic for temperatures

--- AppPage.cs
+++ AppPage.cs
@@ -10,7 +10,7 @@ namespace ConversionApp
     {
         public void Run()
         {
-            Console.WriteLine("Welcome to the Conversion App!\nWhat would you like to convert?:" +
+            Console.WriteLine("Welcome to the Conversion App!\n\nWhat would you like to convert?:" +
                               "\n1. Currency\n2. Temperature\n3. Weight");
 
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -31,6 +31,7 @@ namespace ConversionApp
 
         private void ConvertCurrency()
         {
+            Console.Clear();
             Console.WriteLine("Choose currency to convert from:\n1. NOK\n2. EUR\n3. USD");
             int fromCurrencyChoice = Convert.ToInt32(Console.ReadLine());
 
@@ -77,6 +78,7 @@ namespace ConversionApp
                     return;
             }
 
+            Console.Clear();
             double convertedAmount = ConvertCurrency(fromAmount, fromCurrencyName, toCurrencyName);
             Console.WriteLine($"{fromAmount} {fromCurrencyName} is {convertedAmount} {toCurrencyName}.");
         }
@@ -91,15 +93,15 @@ namespace ConversionApp
         {
             if (fromCurrency == "NOK" && toCurrency == "EUR")
                 return 0.088301199;
-            else if (fromCurrency == "EUR" && toCurrency == "NOK")
+            if (fromCurrency == "EUR" && toCurrency == "NOK")
                 return 11.325047;
-            else if (fromCurrency == "NOK" && toCurrency == "USD")
+            if (fromCurrency == "NOK" && toCurrency == "USD")
                 return 0.095209812;
-            else if (fromCurrency == "USD" && toCurrency == "NOK")
+            if (fromCurrency == "USD" && toCurrency == "NOK")
                 return 10.503119;
-            else if (fromCurrency == "EUR" && toCurrency == "USD")
+            if (fromCurrency == "EUR" && toCurrency == "USD")
                 return 1.0782499;
-            else if (fromCurrency == "USD" && toCurrency == "EUR")
+            if (fromCurrency == "USD" && toCurrency == "EUR")
                 return 0.92742876;
 
             return 1.0;
@@ -107,7 +109,89 @@ namespace ConversionApp
 
         private void ConvertTemperature()
         {
+            Console.Clear();
+            Console.WriteLine("Choose temperature to convert from:\n1. Celsius\n2. Fahrenheit\n3. Kelvin");
+            int fromTemperatureChoice = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Choose temperature to convert to:\n1. Celsius\n2. Fahrenheit\n3. Kelvin");
+            int toTemperatureChoice = Convert.ToInt32(Console.ReadLine());
+
+            double initialTemperatureAmount;
+            string fromTemperatureName;
+            string toTemperatureName;
+
+            Console.Write("Enter amount to convert: ");
+            initialTemperatureAmount = Convert.ToDouble(Console.ReadLine());
+
+            switch (fromTemperatureChoice)
+            {
+                case 1:
+                    fromTemperatureName = "Celsius";
+                    break;
+                case 2:
+                    fromTemperatureName = "Fahrenheit";
+                    break;
+                case 3:
+                    fromTemperatureName = "Kelvin";
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    return;
+            }
+
+            switch (toTemperatureChoice)
+            {
+                case 1:
+                    toTemperatureName = "Celsius";
+                    break;
+                case 2:
+                    toTemperatureName = "Fahrenheit";
+                    break;
+                case 3:
+                    toTemperatureName = "Kelvin";
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    return;
+            }
+
+            Console.Clear();
+            double convertedAmount = ConvertTemperature(initialTemperatureAmount, fromTemperatureName, toTemperatureName);
+            Console.WriteLine($"{initialTemperatureAmount} {fromTemperatureName} is {convertedAmount} {toTemperatureName}.");
+        }
+
+        private double ConvertTemperature(double initTempAmount, string fromTempName, string toTempName)
+        {
+            double CtoF = initTempAmount * 1.8 + 32;
+            double CtoK = initTempAmount + 273.15;
+
+            double FtoC = initTempAmount - 32 / 1.8;
+            double FtoK = initTempAmount + 459.67 * 5 / 9;
+
+            double KtoC = initTempAmount - 273.15;
+            double KtoF = initTempAmount * 9 / 5 - 459.67;
+
+            if (fromTempName == "Celsius" && toTempName == "Fahrenheit")
+            {
+                return CtoF;
+            }
+            if (fromTempName == "Celsius" && toTempName == "Kelvin")
+            {
+                return CtoK;
+            }
+            if (fromTempName == "Kelvin" && toTempName == "Celsius")
+            {
+                return KtoC;
+            }
+            if (fromTempName == "Kelvin" && toTempName == "Fahrenheit")
+            {
+                return KtoF;
+            }
+            if (fromTempName == "Fahrenheit" && toTempName == "Celsius")
+            {
+                return FtoC;
+            }
+            return FtoK;
         }
 
         private void ConvertWeight()
